32606e57a9da0a0b3ce8b8ab1aba726dbe6afe15
LiamMakeThing <oldtown.support@gmail.com>
2020-06-20 14:35:14 -0400

    Museum layout version 1 complete

--- PF_Game/Assets/Code/Utility/GridSnap.cs
+++ PF_Game/Assets/Code/Utility/GridSnap.cs
@@ -8,6 +8,7 @@ public class GridSnap : MonoBehaviour
     [SerializeField] float rotSnapSize = 90.0f;
     Vector3 snapPos;
     Vector3 snapRot;
+    [SerializeField] bool snapRotEnabled;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +18,24 @@ public class GridSnap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Application.isPlaying)
+        {
+            return;
+        }
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSnapSize) * gridSnapSize;
         snapPos.y = Mathf.RoundToInt(transform.position.y / gridSnapSize) * gridSnapSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSnapSize) * gridSnapSize;
         transform.position = snapPos;
 
         //Add rotation snap. 90 for now
+        if (snapRotEnabled)
+        {
+
+        
         snapRot.x = Mathf.RoundToInt(transform.eulerAngles.x / rotSnapSize) * rotSnapSize;
         snapRot.y = Mathf.RoundToInt(transform.eulerAngles.y / rotSnapSize) * rotSnapSize;
         snapRot.z = Mathf.RoundToInt(transform.eulerAngles.z / rotSnapSize) * rotSnapSize;
         transform.eulerAngles = snapRot;
+        }
     }
 }
